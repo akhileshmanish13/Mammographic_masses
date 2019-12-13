8e5e7beef7f61e29c551afeeeab2ad8f2982c639
Gurjas Singh <gurjassingh92@gmail.com>
2019-12-13 11:46:14 +0530

    Changes Updated. Project Working
    Signed-off-by: Gurjas Singh <gurjassingh92@gmail.com>

--- mammographic prediction/mass.py
+++ mammographic prediction/mass.py
@@ -8,7 +8,7 @@ def welcome():
     return render_template('index.html')
 
 def Ad_Severity(age,shape,margin,density):
-    model = joblib.load('masses_model.sav')
+    model = joblib.load('mammo_model.sav')
     predictions = model.predict([[age,shape,margin,density]])
     return predictions
 
@@ -20,13 +20,11 @@ def logistic():
         age = values['Age']
         shape = values['shape']
         margin = values['margin']
-        density= values['density']
+        density = values['density']
         age = int(age)
         shape = int(shape)
         margin = int(margin)
         density = int(density)
-
-
 
         Severity = Ad_Severity(age,shape,margin,density)
 
